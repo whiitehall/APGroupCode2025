12a088a76623b8e330deb04b6a372e95e9f8bdf1
whiitehall <sophie.foenander@gmail.com>
2025-08-21 11:06:38 +1000

    added first 2 pages, still need to edit layout and stuff

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "Start Text":return img`
+..............................
+..............................
+..............................
+.111.111..1...111..111........
+1.....1..1.1..1..1..1.........
+1.....1..1.1..1..1..1.........
+.111..1.1...1.111...1.........
+....1.1.11111.1..1..1.........
+....1.1.1...1.1..1..1.........
+.111..1.1...1.1..1..1.........
+..............................
+..............................
+..............................
+..............................
+..............................
+..............................
+`;
+            case "image1":
+            case "A Button":return img`
+. . . . f f f f f f f f . . . . 
+. . . f 8 8 8 8 8 8 8 8 f . . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. f 8 8 8 8 8 f f 8 8 8 8 8 f . 
+f 8 8 8 8 8 f 8 8 f 8 8 8 8 8 f 
+f 8 8 8 8 8 f 8 8 f 8 8 8 8 8 f 
+f 8 8 8 8 f 8 8 8 8 f 8 8 8 8 f 
+f 8 8 8 8 f 8 8 8 8 f 8 8 8 8 f 
+f 8 8 8 8 f f f f f f 8 8 8 8 f 
+f 8 8 8 8 f 8 8 8 8 f 8 8 8 8 f 
+f 8 8 8 8 f 8 8 8 8 f 8 8 8 8 f 
+. f 8 8 8 f 8 8 8 8 f 8 8 8 f . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . . f 8 8 8 8 8 8 8 8 f . . . 
+. . . . f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
